d63658ddb69e8223937f049b8e3e5417c2fb8a3f
chaitanyagit25 <chaitanyagiradkar53@gmail.com>
2026-07-05 21:59:10 +0530

    add twosum - hashmap solution

---
+++ HashMap/twosum.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+using namespace std;
+vector<int> twosum(vector<int>& nums,int target) {
+    
+    unordered_map<int,int> mp;
+    for(int i=0;i<nums.size();i++){
+        int need = target - nums[i];
+        if(mp.count(need)){
+            
+            return {mp[need],i};
+            
+        }
+        mp[nums[i]]=i;
+
+    }
+    return {};
+}
+int main (){
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> ans = twosum(nums,target);
+    cout<< ans[0] << " " << ans[1] <<endl;
+    return 0;
+}
