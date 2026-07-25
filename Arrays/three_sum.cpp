34313e4e09a1d3e614859c98d4b96b272178316b
chaitanyagit25 <chaitanyagiradkar53@gmail.com>
2026-07-25 23:13:52 +0530

    add file

--- Arrays/three_sum.cpp
+++ Arrays/three_sum.cpp
@@ -4,23 +4,27 @@ class Solution {
     public:
     vector<vector<int>> threesum(vector<int>& nums){
         int n = nums.size();
-        set<vector<int>> st;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
         for(int i =0;i<n;i++){
-            for(int j =i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]+nums[j]+nums[k] == 0){
-                        vector<int> temp = {nums[i],nums[j],nums[k]};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                       
-                    }
-                    
-
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int left = i+1,right = n-1;
+            while(left<right){
+                int sum = nums[i]+ nums[left]+nums[right];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    left ++;
+                    right --;
+                    while(left<right && nums[left] == nums[left-1]) left++;
+                    while(left<right && nums[right]== nums[right +1]) right --;
                 }
+                else if(sum<0) left++;
+                else right --;
+
             }
         }
-        vector<vector<int>> ans(st.begin(),st.end());
         return ans;
+        
     }
 };
 int main(){
