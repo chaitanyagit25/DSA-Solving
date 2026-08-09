708a6ac41882ce0e1e3fcb20b2133db64ba9d012
chaitanyagit25 <chaitanyagiradkar53@gmail.com>
2026-08-09 21:29:15 +0530

    add file

---
+++ Arrays/binary_search/searchin_2dmat2.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+    bool searchele(vector<vector<int>>& matrix,int target){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int row =0;
+        int col = m-1;
+        while(row<n && col >=0){
+                if(matrix[row][col]==target){
+                    return true;
+                }
+                else if(matrix[row][col]<target){
+                    row++;
+                }
+                else{
+                    col--;
+                }
+
+            }
+        return false;
+    }
+};
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    Solution sol;
+    if(sol.searchele(matrix,22)){
+        cout<<"true\n";
+    }
+    else{
+        cout<<"false\n";
+    }
+    return 0;
+
+}
