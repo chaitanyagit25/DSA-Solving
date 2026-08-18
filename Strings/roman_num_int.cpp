573eb0ffaee6afe10ff7e25d26fe451578f9ba6c
chaitanyagit25 <chaitanyagiradkar53@gmail.com>
2026-08-18 21:21:51 +0530

    add file

---
+++ Strings/roman_num_int.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    int romantoint(string s){
+        unordered_map<char,int> value= {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+
+        };
+        int ans = 0;
+        for(int i  = 0; i <s.size() ;i++){
+            if(i < s.size()-1) {
+                if(value[s[i]] < value[s[i+1]]){
+                    ans -= value[s[i]];
+                }
+                else {
+                    ans +=  value[s[i]];
+                    
+                }
+            }
+            else {
+                ans += value[s[i = s.size()-1]];
+            }
+
+        }
+        return ans;
+    }
+};
+int main() {
+    Solution sol;
+    string s = "MCMXCIV"; 
+    int result = sol.romantoint(s);
+    
+    
+    cout << "Integer value: " << result << endl;
+
+    return 0;
+}
