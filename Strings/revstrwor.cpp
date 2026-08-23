4084d662d523e95a1b3db7e83c546684d5857241
chaitanyagit25 <chaitanyagiradkar53@gmail.com>
2026-08-23 22:06:03 +0530

    optimal for reverse words in a string

---
+++ Strings/revstrwor.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    string revwor(string s){
+        string result = "";
+        int i = s.size()-1;
+        while(i>=0){
+            while(i>=0 && s[i] == ' '){
+                i--;
+            }
+            if(i<0) break;
+            int end = i ;
+            while(i >= 0 && s[i] != ' '){
+                i--;
+            }
+            string word = s.substr(i+1,end-i);
+            if(!result.empty()){
+                result += " ";
+            }
+            result += word;
+        }
+        return result;
+    }
+};
+int main(){
+    Solution sol;
+    string s = "hii how are you";
+    cout<<sol.revwor(s)<<endl;
+    return 0;
+}
