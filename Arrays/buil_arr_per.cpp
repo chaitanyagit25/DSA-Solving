8068a8b2bcb8b58219ec0fab645fab2bb32998bc
chaitanyagit25 <chaitanyagiradkar53@gmail.com>
2026-08-26 23:37:16 +0530

    add file

---
+++ Arrays/buil_arr_per.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    vector<int> arrper(vector<int>& nums){
+        vector<int> ans;
+        for(int i = 0 ;i<nums.size();i++){
+            ans.push_back(nums[nums[i]]);
+        }
+        return ans;
+    }
+};
+int main(){
+    Solution sol;
+    vector<int> nums ={0,2,1,5,3,4};
+    vector<int> res = sol.arrper(nums);
+    for(auto it : res){
+        cout<<it<<" ";
+    }
+    return 0;
+}
